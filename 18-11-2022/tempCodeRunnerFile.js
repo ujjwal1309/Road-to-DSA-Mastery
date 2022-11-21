f3938708038a3e1a5528728c51842fb7a0da6ff1
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-11-21 23:19:13 +0530

    done

--- 18-11-2022/tempCodeRunnerFile.js
+++ 18-11-2022/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-    // for(let i of arr){
-    //     let count=1;
-    //     for(let j=0; j<i; j++)
-    //     {
-    //         if(arr[j]<arr[i]){
-    //             count++;
-    //         }
-    //     }
-    // }
-    // ans+=`${count} `;
+    // e == 5 ? (flag = false) : (flag = true);
