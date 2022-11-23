23bd3c7bbd5e0f4439b8086fa8116fa4760c7066
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-11-24 00:01:49 +0530

    done

--- 23-11-2022/5.js
+++ 23-11-2022/5.js
@@ -1,9 +1,37 @@
-function runProgram(input) {
-    // Write code here
-   
+function balanced(str, n) {
+  let stack = [],
+    i = 0;
+
+  while (i < n) {
+    if (str[i] === str[i].toLowerCase()) {
+      stack.push(str[i].toUpperCase());
+    } else if (stack[stack.length - 1] === str[i]) {
+      stack.pop();
+    } else {
+      console.log("False");
+      return;
+    }
+    i++;
   }
-  if (process.env.USERNAME=== "ujjwa") {
-    runProgram(`5
+
+  stack.length ? console.log("False") : console.log("True");
+}
+
+function runProgram(input) {
+  // Write code here
+  input = input.trim().split("\n");
+  let tc = +input[0];
+  let line = 1;
+  for (let i = 0; i < tc; i++) {
+    let n = +input[line];
+    line++;
+    let str = input[line].trim();
+    line++;
+    balanced(str, n);
+  }
+}
+if (process.env.USERNAME === "ujjwa") {
+  runProgram(`5
     2
     aA
     6
@@ -14,21 +42,21 @@ function runProgram(input) {
     caCA
     4
     daAD`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0) ;
-    });
-  }
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
