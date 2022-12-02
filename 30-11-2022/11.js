728208bb2662e41c09fd346533763c4e2959d448
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-12-02 23:10:44 +0530

    practice made

--- 30-11-2022/11.js
+++ 30-11-2022/11.js
@@ -11,6 +11,8 @@ class linkedList {
     this.size = 0;
   }
 
+  //------------------------------------------------------------------------------------------->
+
   add(el) {
     let node = new Node(el);
     if (!this.head) {
@@ -25,6 +27,96 @@ class linkedList {
 
     this.size++;
   }
+
+  //------------------------------------------------------------------------------------------->
+
+  print() {
+    let current = this.head;
+    let i = 0;
+    while (current) {
+      console.log(current.el, i++);
+      current = current.next;
+    }
+  }
+
+  //------------------------------------------------------------------------------------------->
+
+  isEmpty() {
+    this.size ? console.log(false) : console.log(true);
+  }
+
+  //------------------------------------------------------------------------------------------->
+
+  insertAt(el, index) {
+    //if index is gretaer than size, index is negative,size is 0
+
+    if (index > this.size && index < 0 && this.size) {
+      return false;
+    }
+
+    let node = new Node(el);
+    let current = this.head;
+    let prev = null;
+    let i = 0;
+    if (index == 0) {
+      node.next = current;
+      this.head = node;
+    } else {
+      while (i < index) {
+        prev = current;
+        current = current.next;
+        i++;
+      }
+
+      //insert at the index
+
+      prev.next = node;
+
+      // move to the inserted node
+
+      prev = prev.next;
+
+      // add the rest of the current to the inserted node
+
+      prev.next = current;
+
+      //increment size
+    }
+
+    this.size++;
+  }
+
+  //------------------------------------------------------------------------------------------->
+
+  delete(el) {
+    let current = this.head;
+    let prev = null;
+
+    while (current) {
+      if (current.el === el) {
+        //do something
+
+        if (prev == null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+      }
+
+      prev = current;
+      current = current.next;
+      this.size--
+    }
+  }
 }
 
 let l = new linkedList();
+l.add(1);
+l.isEmpty();
+l.add(2);
+l.add(3);
+l.add(4);
+l.insertAt(15, 0);
+l.print();
+
+console.log(l);
