379efe64b902a0ba6158f3539791f6ac426faa45
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-12-08 23:30:29 +0530

    Practice done

---
+++ 8-12-2022/2.js
@@ -0,0 +1,49 @@
+function possiblePalindrome(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    !obj[str[i]] ? (obj[str[i]] = 1) : obj[str[i]]++;
+  }
+
+  let count = 0;
+
+  for (let i in obj) {
+    if (obj[i] % 2 == 0) continue;
+
+    count++;
+  }
+
+  count <= 1 ? console.log("Yes") : console.log("No");
+}
+
+function runProgram(input) {
+  // Write code here
+  input = input.trim().split("\n");
+  let tc = +input[0];
+  let line = 1;
+  for (let i = 0; i < tc; i++) {
+    let str = input[line].trim();
+    line++;
+    possiblePalindrome(str);
+  }
+}
+if (process.env.USERNAME === "ujjwa") {
+  runProgram(`1
+    carerca`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
---
+++ 8-12-2022/4.js
@@ -0,0 +1,53 @@
+function buySomeHouses(arr,n){
+    let obj=new Set();
+
+    for(let i=0; i<n; i++){
+        for(let j=0; j<arr[0].length; j++){
+            obj.add(arr[i][j]);
+        }
+    }
+
+    console.log(obj.size)
+}
+
+function runProgram(input) {
+    // Write code here
+    input = input.trim().split("\n");
+    let tc = +input[0];
+    let line = 1;
+    for (let i = 0; i < tc; i++) {
+      let n = +input[line];
+      line++;
+      let arr = new Array(n);
+      for (let i = 0; i < n; i++) {
+        arr[i] = input[line].trim().split(" ").map(Number);
+        line++;
+      }
+      buySomeHouses(arr, n);
+    }
+  }
+  if (process.env.USERNAME === "ujjwa") {
+    runProgram(`1
+      3
+      1 2
+      2 3
+      1 3
+      `);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
