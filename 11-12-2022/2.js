b515f017afcb413cea78d72bc38cc460c6c41807
ujjwal1309 <ujjwalsharma1309@gmail.com>
2022-12-11 18:22:33 +0530

    Practice for eval

---
+++ 11-12-2022/2.js
@@ -0,0 +1,65 @@
+function gamblingAddict(arr,n){
+    let s=[];
+    let i=n-1;
+    let ans=new Array(n);
+
+    while(i>=0){
+
+        let count=1
+
+        while(s.length && arr[s[s.length-1]]<=arr[i]){
+            count+=ans[s[s.length-1]];
+            s.pop();
+        }
+
+        !s.length ? ans[i]=0 : ans[i]=count;
+
+        s.push(i);
+        i--;
+
+    }
+
+    console.log(ans.join(' '))
+
+}
+
+function runProgram(input) {
+    // Write code here
+    input=input.trim().split('\n');
+    let tc=+input[0];
+    let line=1;
+    for(let i=0; i<tc; i++){
+        let n=input[line];
+        line++;
+        let arr=input[line].trim().split(' ').map(Number);
+        line++;
+        gamblingAddict(arr,n);
+    }
+   
+  }
+  if (process.env.USERNAME=== "ujjwa") {
+    runProgram(`3
+    8
+    73 74 75 71 69 72 76 73
+    4
+    30 40 50 60
+    3
+    30 60 90`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
